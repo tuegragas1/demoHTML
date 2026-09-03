90a4b1037ee395092e16778d42d9b5a05f294393
tuegragas1 <tuegragas1@gmail.com>
2026-09-03 20:17:52 +0700

    baitapdasua

---
+++ js/my.js
@@ -0,0 +1,5 @@
+let n = prompt("Nhap gia tri: ", 0)
+let b = prompt("Nhap gia tri: ", 1)
+let c = prompt("Nhap gia tri: ", 2)
+let tong = ((Number(n) + Number(b) + Number(c))) / 3
+document.getElementById("kq").innerHTML = "<h1>Diemtb: " + tong + "</h1>"
