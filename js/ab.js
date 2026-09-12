4475628ef1f1d7ecc88bb5de81860272fc616409
tuegragas1 <tuegragas1@gmail.com>
2026-09-12 21:33:51 +0700

    cap nhat

--- js/ab.js
+++ js/ab.js
@@ -1,5 +1,17 @@
+/*
 let n = Number(5);
 for (let i = 0; i <= n; i++) {
   document.writeln("The number is " + i + "<br>")
-}
+}*/
 
+let a = Number(prompt("Nhap so bat ky "));
+let sum = 0;
+if (a <= 0) {
+  document.writeln("số nhập phải lớn hơn không")
+}
+else {
+  for (let b = 1; b <= a; b++) {
+    sum = sum + b;
+  }
+  document.writeln("The sum of " + a + " is " + sum);
+}
