623336f85ffa403be52f2585d4fd80006afe5ac1
tuegragas1 <tuegragas1@gmail.com>
2026-09-12 22:27:36 +0700

    sua lai

--- js/ab.js
+++ js/ab.js
@@ -16,7 +16,7 @@ else {
   document.writeln("The sum of " + a + " is " + sum);
 }
 */
-/*
+
 let n = Number(prompt("nhap so vao hop thoai "));
 let sum = 0;
 while (n != -1) {
@@ -25,9 +25,10 @@ while (n != -1) {
   n = Number(prompt("nhap so vao hop thoai "));
 }
 document.writeln("tong cac so la" + sum);
-*/
+
+
 let i = 1;
 while (i <= 100) {
-  document.writeln("<hr width= '" + i + "%><br>");
+  document.writeln("<hr width= '" + i + "%'><br>");
   i++;
 }
