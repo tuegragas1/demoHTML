dfb5dc6aa874375409f765c774f3d3b970f45239
tuegragas1 <tuegragas1@gmail.com>
2026-09-12 22:18:22 +0700

    cap nhat

--- js/ab.js
+++ js/ab.js
@@ -3,7 +3,7 @@ let n = Number(5);
 for (let i = 0; i <= n; i++) {
   document.writeln("The number is " + i + "<br>")
 }*/
-
+/*
 let a = Number(prompt("Nhap so bat ky "));
 let sum = 0;
 if (a <= 0) {
@@ -15,3 +15,12 @@ else {
   }
   document.writeln("The sum of " + a + " is " + sum);
 }
+*/
+let n = Number(prompt("nhap so vao hop thoai "));
+let sum = 0;
+while (n != -1) {
+  sum = n + sum;
+  document.writeln("So da nhap " + n + "<br>");
+  n = Number(prompt("nhap so vao hop thoai "));
+}
+document.writeln("tong cac so la" + sum);
